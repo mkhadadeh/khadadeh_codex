3e110146efe4036d7a68fb4bd876ba0acf93e592
Ellen Tseng <33066419+elltseng@users.noreply.github.com>
2018-10-18 20:45:11 -0700

    Adding structures

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    private bool piece_disk;
+    private bool piece_holder;
+    private bool piece_swoop;
+    private bool piece_swish;
+    private bool piece_top;
+
+    // Use this for initialization
+    void Start () {
+		piece_disk = false;
+        piece_holder = false;
+        piece_swoop = false;
+        piece_swish = false;
+        piece_top = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
