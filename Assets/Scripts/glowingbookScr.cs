4b9588211a0bd87fabba39a7fd3fe5dd81d040eb
Ellen Tseng <33066419+elltseng@users.noreply.github.com>
2018-10-25 18:35:11 -0700

    Glowing books!

---
+++ Assets/Scripts/glowingbookScr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glowingbookScr : MonoBehaviour {
+
+    public Material book_mat;
+    public Gradient particle_grad;
+    // Use this for initialization
+    void Start () {
+        gameObject.transform.GetChild(2).GetComponent<Renderer>().material = book_mat;
+        var col = gameObject.transform.GetChild(0).GetComponent<ParticleSystem>().colorOverLifetime;
+        col.color = particle_grad;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
