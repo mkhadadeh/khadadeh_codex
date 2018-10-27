71659ddd95ff7c4fbecc3cd57e981c48ea991de1
Chewbacca <scu.imaginarium@gmail.com>
2018-10-26 19:49:20 -0700

    Finger Guns!

---
+++ Assets/Scripts/particles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(-(transform.parent.rotation.eulerAngles.x + 90), -transform.parent.rotation.eulerAngles.y, -(transform.parent.rotation.eulerAngles.z + 270));
+	}
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -23,15 +23,38 @@ public class player : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+		if(!OVRInput.Get(OVRInput.Touch.PrimaryIndexTrigger) && OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) == 1.0f)
         {
-            Structure.GetComponent<structure>().piece_disk = piece_disk;
-            Structure.GetComponent<structure>().piece_holder = piece_holder;
-            Structure.GetComponent<structure>().piece_swoop = piece_swoop;
-            Structure.GetComponent<structure>().piece_swish = piece_swish;
-            Structure.GetComponent<structure>().piece_top = piece_top;
+            RaycastHit hit;
+            Ray pointRay = new Ray(transform.GetChild(1).GetChild(0).GetChild(4).position, transform.GetChild(1).GetChild(0).GetChild(4).forward);
+            if (Physics.Raycast(pointRay, out hit)) {
+                if (hit.collider.tag == "Pedestal")
+                {
+                    updatePedestal();
+                }
+            }
         }
-	}
+        if (!OVRInput.Get(OVRInput.Touch.SecondaryIndexTrigger) && OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) == 1.0f)
+        {
+            RaycastHit hit;
+            Ray pointRay = new Ray(transform.GetChild(1).GetChild(0).GetChild(5).position, transform.GetChild(1).GetChild(0).GetChild(5).forward);
+            if (Physics.Raycast(pointRay, out hit))
+            {
+                if (hit.collider.tag == "Pedestal")
+                {
+                    updatePedestal();
+                }
+            }
+        }
+    }
+
+    void updatePedestal(){
+        Structure.GetComponent<structure>().piece_disk = piece_disk;
+        Structure.GetComponent<structure>().piece_holder = piece_holder;
+        Structure.GetComponent<structure>().piece_swoop = piece_swoop;
+        Structure.GetComponent<structure>().piece_swish = piece_swish;
+        Structure.GetComponent<structure>().piece_top = piece_top;
+    }
 
     public void collect(string piece_tag) {
         switch(piece_tag)
