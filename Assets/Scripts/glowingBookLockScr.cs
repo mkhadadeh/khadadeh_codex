925b428a6e5f74f28dd98261f00973985fd084e8
Ellen Tseng <33066419+elltseng@users.noreply.github.com>
2018-11-01 18:48:39 -0700

    Fixed dome and added book locks

---
+++ Assets/Scripts/glowingBookLockScr.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glowingBookLockScr : MonoBehaviour {
+    public GameObject book;
+    public GameObject door;
+
+    private Vector3 book_pos;
+    private bool locked;
+
+    // Use this for initialization
+    void Start () {
+        book_pos = book.transform.position;
+        locked = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Vector3.Distance(book_pos, book.transform.position));
+		if(locked && Vector3.Distance(book_pos, book.transform.position) >= 1)
+        {
+            unlock();
+        }
+	}
+    
+    void unlock()
+    {
+        Destroy(door);
+        Destroy(gameObject);
+    }
+}
