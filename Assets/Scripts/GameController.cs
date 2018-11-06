2d0e6adc8a8574f0f02590f66738e118c73953ba
Ellen Tseng <33066419+elltseng@users.noreply.github.com>
2018-11-06 12:22:19 -0800

    Ui Started

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
     public GameObject spot_light;
+    public GameObject all_light;
 
 	// Use this for initialization
 	void Start () {
@@ -20,5 +21,6 @@ public class GameController : MonoBehaviour {
     {
         Debug.Log("Victory!");
         spot_light.SetActive(true);
+        all_light.SetActive(false);
     }
 }
---
+++ Assets/Scripts/loadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour {
+
+    public void ChangeScene()
+    {
+        //Changes to game scene
+        Debug.Log("Change Scene");
+        SceneManager.LoadScene("Main Scene");
+    }
+}
